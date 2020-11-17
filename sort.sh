4829100d3fbfca2a78d4efab1139158f823d3a19
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-11-17 20:00:09 +0530

    Added: store result in a dictionary for every computation

--- sort.sh
+++ sort.sh
@@ -2,7 +2,13 @@ echo "Arithmetic Computation and Sorting"
 read -p "enter a num: " a
 read -p "enter a num: " b
 read -p "enter a num: " c
-echo "a+b*c=$(($a+$b*$c))" 
-echo "a*b+c=$(($a*$b+$c))"
-echo "c+a/b=$(($c+$a/$b))"
-echo "a%b+c=$(($a%$b+$c))"
+declare -A  arithmetic
+arithmetic[0]=$(($a+$b*$c))
+arithmetic[1]=$(($a*$b+$c))
+arithmetic[2]=$(($c+$a/$b))
+arithmetic[3]=$(($a%$b+$c))
+echo "a+b*c=${arithmetic[0]}" 
+echo "a*b+c=${arithmetic[1]}"
+echo "c+a/b=${arithmetic[2]}"
+echo "a%b+c=${arithmetic[3]}"
+echo "${arithmetic[@]}"
